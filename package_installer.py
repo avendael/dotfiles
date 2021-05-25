56654ee058982ec413855672e316afe2e5beca53
Evan Aromin <evan@avendael.com>
2021-05-26 07:45:22 +0800

    Use dotbot_plugin_aptget for managing ubuntu packages

--- package_installer.py
+++ package_installer.py
@@ -3,10 +3,8 @@ import os
 sysname = os.uname()[0]
 is_known_system = True
 
-if sysname == 'Linux':
-    from platforms.ubuntu import install_dependencies, install_packages
-elif sysname == 'Darwin':
-    from platformns.macos import install_dependencies, install_packages
+if sysname == 'Darwin':
+    from platforms.macos import install_dependencies, install_packages
 else:
     print('Unknown system. No packages will be installed')
     is_known_system = False
