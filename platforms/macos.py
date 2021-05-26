c3bc6bb8624fde7b470298f70ee3683f40c03e5b
Evan Aromin <evan@avendael.com>
2021-05-26 08:28:07 +0800

    Fix brew python script

--- platforms/macos.py
+++ platforms/macos.py
@@ -3,58 +3,58 @@ import os
 from subprocess import call, check_output
 
 brews = set([
-    'chkrootkit',
-    'ctags',
-    'fasd',
-    'fzf',
-    'git',
-    'jq',
-    'neovim',
-    'pyenv',
-    'reattach-to-user-namespace',
-    'rkhunter',
-    'telnet',
-    'the_silver_searcher',
-    'tmux',
-    'unrar',
-    'zplug',
-    'zsh',
+    b'chkrootkit',
+    b'ctags',
+    b'fasd',
+    b'fzf',
+    b'git',
+    b'jq',
+    b'neovim',
+    b'pyenv',
+    b'reattach-to-user-namespace',
+    b'rkhunter',
+    b'telnet',
+    b'the_silver_searcher',
+    b'tmux',
+    b'unrar',
+    b'zplug',
+    b'zsh',
 ])
 taps = set([
-    'homebrew/cask-drivers',
-    'homebrew/cask-fonts',
-    'beeftornado/rmtree',
+    b'homebrew/cask-drivers',
+    b'homebrew/cask-fonts',
+    b'beeftornado/rmtree',
 ])
 casks = set([
-    'alfred',
-    'anki',
-    'authy',
-    'bartender',
-    'bisq',
-    'discord',
-    'docker',
-    'dropbox',
-    'font-hack',
-    'firefox',
-    'google-chrome',
-    'gpg-suite',
-    'imageoptim',
-    'iterm2',
-    'java',
-    'keepingyouawake',
-    'keybase',
-    'licecap',
-    'little-snitch',
-    'logitech-options',
-    'slack',
-    'skype',
-    'spotify',
-    'steam',
-    'telegram',
-    'transmission',
-    'viscosity',
-    'visual-studio-code',
-    'vlc',
+    b'alfred',
+    b'anki',
+    b'authy',
+    b'bartender',
+    b'bisq',
+    b'discord',
+    b'docker',
+    b'dropbox',
+    b'font-hack',
+    b'firefox',
+    b'google-chrome',
+    b'gpg-suite',
+    b'imageoptim',
+    b'iterm2',
+    b'java',
+    b'keepingyouawake',
+    b'keybase',
+    b'licecap',
+    b'little-snitch',
+    b'logitech-options',
+    b'slack',
+    b'skype',
+    b'spotify',
+    b'steam',
+    b'telegram',
+    b'transmission',
+    b'viscosity',
+    b'visual-studio-code',
+    b'vlc',
 ])
 pips = set([
     # 'tmuxp',
@@ -73,9 +73,9 @@ def install_dependencies():
     if not has_brew:
         print('Installing homebrew...')
         os.system(
-            '/usr/bin/ruby -e '
+            '/bin/bash -c '
             '"$(curl -fsSL '
-            'https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+            'https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
         )
     else:
         print('Homebrew found. Ready to brew!')
@@ -83,7 +83,9 @@ def install_dependencies():
 
 
 def install_packages():
-    installed_brews = set(check_output(['brew', 'list', '--formula']).strip().split(b'\n'))
+    installed_brews = set(
+        check_output(['brew', 'list', '--formula']).strip().split(b'\n')
+    )
     installed_taps = set(check_output(['brew', 'tap']).strip().split(b'\n'))
     installed_casks = set(
         check_output(['brew', 'list', '--cask']).strip().split(b'\n')
@@ -98,22 +100,30 @@ def install_packages():
     pips_to_install = pips - installed_pips
 
     if taps_to_install:
-        print('Taps to install: {}'.format(', '.join(taps_to_install)))
+        print('Taps to install: {}'.format(
+            ', '.join([str(i, 'utf-8') for i in taps_to_install]))
+        )
     else:
         print('No taps to install')
 
     if brews_to_install:
-        print('Brews to install: {}'.format(', '.join(brews_to_install)))
+        print('Brews to install: {}'.format(
+            ', '.join([str(i, 'utf-8') for i in brews_to_install]))
+        )
     else:
         print('No brews to install')
 
     if casks_to_install:
-        print('Casks to install: {}'.format(', '.join(casks_to_install)))
+        print('Casks to install: {}'.format(
+            ', '.join([str(i, 'utf-8') for i in casks_to_install]))
+        )
     else:
         print('No casks to install')
 
     if pips_to_install:
-        print('Pips to install: {}'.format(', '.join(pips_to_install)))
+        print('Pips to install: {}'.format(
+            ', '.join([str(i, 'utf-8') for i in pips_to_install]))
+        )
     else:
         print('No pips to install')
 
